f8f18063191a78f5aecb3d57362538cf191d9831
Kjell <kjell@leanside.com>
2014-04-10 10:42:59 -0500

    use $routeParams to get object key

--- js/controllers.js
+++ js/controllers.js
@@ -4,23 +4,18 @@ angular.module('myApp.controllers', [])
   .controller('homeCtrl', ['$scope', 'objects', function($scope, objects) {
     $scope.objects = objects
   }])
-  .controller('ListenCtrl', ['$scope', '$location', '$rootScope', 'AudioSources', 'objects', function($scope, $location, $rootScope, AudioSources, objects) {
+  .controller('ListenCtrl', ['$scope', '$location', '$rootScope', '$routeParams', 'AudioSources', 'objects', function($scope, $location, $rootScope, $routeParams, AudioSources, objects) {
   if($rootScope.audioSources) $rootScope.audioSources.pause()
   $rootScope.audioSources = $scope.audioSources = AudioSources;
 
-  var key = $location.path().slice(1)
-
-  // ^^^ can't use routes controller, which is the usual way to do this,
-  // because people go directly to these URLS
+  var key = $routeParams.key
 
   $scope.audioSources.load(key).then(function(x) {
     $scope.object = x
     $scope.tracks = x.titles
   });
-
   // ^^^ this .then thing above is not idiomatic; something about an array
   // of audio buffers messes up directly binding to the view
   // normally, follow the pattern shown in this tutorial:
   // http://markdalgleish.com/2013/06/using-promises-in-angularjs-views/
-
 }]);
